a1ceb8926c0885a64f143228d88a6942756bc549
emthos <thosinnhilmi@gmail.com>
2021-10-13 13:52:19 +0700

    tambah kolom dan menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -52,6 +52,10 @@
                                 </li>
                             @endif
                         @else
+
+                            <a href="/home" class="btn btnwarning">Home</a>
+                            <a href="/students" class="btn btnwarning">CRUD</a>
+
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
